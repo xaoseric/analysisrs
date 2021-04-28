b8d038ed7ece6bdc73e2aec471ad6f6e9cc50e72
Eric Hocking <ehock7229@outlook.com>
2021-04-28 15:30:27 -0400

    adds ability to skip running data exports

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate diesel;
 extern crate dotenv;
 extern crate csv;
 extern crate serde;
+extern crate clap;
 
 pub mod models;
 pub mod schema;
@@ -14,6 +15,7 @@ use std::env;
 use std::error::Error;
 use models::titanic::{TitanicAnalytic, create_new_titanic_analytic};
 use models::stock_history::{StockData, create_stock_history};
+use clap::{Arg, App};
 
 /// Establishes the connection to teh database
 fn establish_connection() -> MysqlConnection {
@@ -76,35 +78,77 @@ fn read_stock_file(path: &str, ticker: &str, conn: &MysqlConnection) -> Result<(
 }*/
 
 fn main() {
+    let mut titanic_analytic_data = true;
+    let mut stock_history_data = true;
+    let mut cruise_analytic_data = true;
+
+    let matches = App::new("Analysisrs")
+        .version("1.0.0")
+        .arg(Arg::with_name("skiptitanic")
+            .long("skiptitanic")
+            .help("Skips processing of titanic csv data")
+            .required(false)
+            .takes_value(false))
+        .arg(Arg::with_name("skipstocks")
+            .long("skipstocks")
+            .help("Skips processing of stock history data")
+            .required(false)
+            .takes_value(false))
+        .arg(Arg::with_name("skipcruise")
+            .long("skipcruise")
+            .help("Skips processing of cruise analytic data")
+            .required(false)
+            .takes_value(false))
+        .get_matches();
+
+    if matches.is_present("skiptitanic") {
+        titanic_analytic_data = false;
+    }
+
+    if matches.is_present("skipstocks") {
+        stock_history_data = false;
+    }
+
+    if matches.is_present("skipcruise") {
+        cruise_analytic_data = false;
+    }
+
     println!("Connecting to database...");
 
     // connect to database
     let connection = establish_connection();
 
-    // read data from csv files and insert into database
-    println!("Inserting Titanic Analysis Data...");
+    if titanic_analytic_data {
+        // read data from csv files and insert into database
+        println!("Inserting Titanic Analysis Data...");
 
-    // if an error occurs, print the error
-    if let Err(e) = read_titanic_file("./data/titanic_analytics.csv", &connection) {
-        eprintln!("{}", e);
+        // if an error occurs, print the error
+        if let Err(e) = read_titanic_file("./data/titanic_analytics.csv", &connection) {
+            eprintln!("{}", e);
+        }
+
+        println!("Titanic Analytic Data inserted successfully!");
     }
 
-    println!("Titanic Analytic Data inserted successfully!");
-    println!("Inserting Cruises Analysis Data...");
+    if stock_history_data {
+        println!("Inserting Stock History Data...");
+        if let Err(e) = read_stock_file("./data/stock_CCL_history.csv", "CCL", &connection) {
+            eprintln!("{}", e);
+        }
 
-    println!("Inserting Stock History Data...");
-    if let Err(e) = read_stock_file("./data/stock_CCL_history.csv", "CCL", &connection) {
-        eprintln!("{}", e);
+        if let Err(e) = read_stock_file("./data/stock_RCL_history.csv", "RCL", &connection) {
+            eprintln!("{}", e);
+        }
+
+        if let Err(e) = read_stock_file("./data/stock_NCLH_history.csv", "NCLH", &connection) {
+            eprintln!("{}", e);
+        }
+        println!("Stock History Data inserted successfully!");
     }
 
-    if let Err(e) = read_stock_file("./data/stock_RCL_history.csv", "RCL", &connection) {
-        eprintln!("{}", e);
+    if cruise_analytic_data {
+        println!("Inserting Cruises Analysis Data...");
     }
-
-    if let Err(e) = read_stock_file("./data/stock_NCLH_history.csv", "NCLH", &connection) {
-        eprintln!("{}", e);
-    }
-    println!("Stock History Data inserted successfully!");
 
     //read_cruises_file("");
 }
